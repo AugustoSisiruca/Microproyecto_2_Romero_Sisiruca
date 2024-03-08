50273f8e43cd74da6859ec89282b9a32bbadede0
AugustoSisiruca <sisiruca.augusto@correo.unimet.edu.ve>
2024-03-08 09:49:52 -0400

    Inicio 😉
    Crear React

---
+++ src/router.js
@@ -0,0 +1,24 @@
+import { createBrowserRouter } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
+import HomePage from './pages/HomePage';
+import ParamsPage from './pages/ParamsPage';
+
+export const router = createBrowserRouter([
+{
+    path: '/',
+    element: <HomePage />,
+},
+{
+    path: '/home/:id/:nombre',
+    element: <ParamsPage />,
+},
+{
+    path: '/login',
+    // element: <LoginPage />,
+    Component: LoginPage,
+},
+{
+    path: '/user/profile',
+    element: <div>Perfil de usuario</div>,
+},
+]);
